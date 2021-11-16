921c5d59ecc98c13dcaa2d8707fe9936aad4b497
Viktor Chornopyskyy <viktor@nyhetstjenester.no>
2021-11-16 11:23:10 +0200

    #1 optimization

--- src/services/AccommodationsService.ts
+++ src/services/AccommodationsService.ts
@@ -1,4 +1,4 @@
-import { Accommodation } from '../interfaces/Accommodation';
+import {Accommodation} from '../interfaces/Accommodation';
 import AccommodationsDictionary from '../dictionary_services/AccommodationsDictionary';
 
 class AccommodationsService {
@@ -9,42 +9,27 @@ class AccommodationsService {
     return result;
   }
   static GetListsData() {
-    return new Promise((resolve, reject) => {
-      fetch(`${process.env.PUBLIC_API_URL}/api/v1/returnAccommodationsData`, {
-        method: 'GET',
-        mode: 'cors',
-        cache: 'no-cache',
-      })
-        .then(response => response.json().then((data) => {
-          resolve({
-            AccommodationValues: AccommodationsDictionary.AccommodationValues(data['Accommodation suppliers']),
-            RoomTypeList: AccommodationsDictionary.RoomTypeList(data.dic_room_type),
-            StatusList: AccommodationsDictionary.StatusList(data.dic_status),
-          });
-        })).catch((error) => {
-          reject(error);
-        });
-    });
+    return fetch(`${process.env.PUBLIC_API_URL}/api/v1/returnAccommodationsData`)
+      .then(response => response.json().then(data => ({
+        AccommodationValues: AccommodationsDictionary.AccommodationValues(data['Accommodation suppliers']),
+        RoomTypeList: AccommodationsDictionary.RoomTypeList(data.dic_room_type),
+        StatusList: AccommodationsDictionary.StatusList(data.dic_status),
+      }))).catch(error => error);
   }
   static updatePrices(payload) {
-    const result: Promise<Accommodation> = new Promise((resolve, reject) => {
-      fetch(`${process.env.PUBLIC_API_URL}/api/v1/saveAccommodationsData`, {
-        method: 'POST',
-        mode: 'cors',
-        cache: 'no-cache',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(AccommodationsDictionary.RequestPrices(payload)),
-      }).then(response => response.json().then((data) => {
-        resolve({
-          ...payload,
-          price: parseFloat(data.price),
-          cost: parseFloat(data.cost),
-        });
-      })).catch(error => reject(error));
-    });
-    return result;
+    return fetch(`${process.env.PUBLIC_API_URL}/api/v1/saveAccommodationsData`, {
+      method: 'POST',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(AccommodationsDictionary.RequestPrices(payload)),
+    }).then(response => response.json().then(data => ({
+      ...payload,
+      price: parseFloat(data.price),
+      cost: parseFloat(data.cost),
+    }))).catch(error => (error));
   }
 }
 
